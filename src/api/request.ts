ded38400cfd102a333888f40d49694b9767bbb5b
Gaëtan H <steellgold0@gmail.com>
2023-02-19 19:33:02 +0100

    😶‍🌫️ wtf dude????

--- src/api/request.ts
+++
@@ -1,8 +0,0 @@
-import gqlRequest, { Variables } from 'graphql-request';
-import { api } from '../../resources/secret.json';
-
-export async function request<T>(request: string, variables?: Variables): Promise<T> {
-	return await gqlRequest(api.url, request, variables, {
-		'authorization': 'Bot ' + api.token
-	});
-}
