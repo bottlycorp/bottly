bf84ea0c6969d55b39a279e071e6187775203b16
Gaëtan H <steellgold0@gmail.com>
2023-02-22 21:42:28 +0100

    New server webhook embed

--- src/utils/Embed.ts
+++ src/utils/Embed.ts
@@ -1,15 +1,23 @@
 import { EmbedBuilder } from "discord.js";
 
-type EmbedType = "normal" | "error";
+type EmbedType = "normal" | "error" | "success";
+
+type Colors = {
+  [key in EmbedType]: number;
+};
+
+const colors: Colors = {
+  normal: 0x4353fc,
+  error: 0xfc4343,
+  success: 0x43fc43,
+};
 
 export function simpleEmbed(message: string, type: EmbedType = "normal", title?: string, footer ?: {
   text: string;
   iconURL?: string;
   timestamp?: boolean;
 }) {
-  const color = type === "normal" ? "#4353fc" : "#fc4343";
-
-  const embed = new EmbedBuilder().setColor(color);
+  const embed = new EmbedBuilder().setColor(colors[type]);
 
   if (message) embed.setDescription(message);
   if (title) embed.setTitle(title);
