1cda968dc86668d7cd281634eb1ea88ad8ebd31a
Gaëtan H <steellgold0@gmail.com>
2023-02-24 23:47:04 +0100

    Fix: No wait to bot restart for count users/guilds

--- src/tasks/list/ActivityUpdate.ts
+++ src/tasks/list/ActivityUpdate.ts
@@ -1,13 +1,14 @@
 import Client from "$core/Client";
 import Task from "$core/tasks/Task";
+import { formatNumbers } from "$core/utils/Number";
 import { ActivityType } from "discord.js";
 
 const Presences = [{
-    name: "GPT-3",
-    type: "PLAYING"
-  }, {
-    name: "{SERVER_COUNT} servers | {USER_COUNT} users",
-    type: "WATCHING"
+  name: "GPT-3",
+  type: "PLAYING"
+},{
+  name: "{SERVER_COUNT} servers | {USER_COUNT} users",
+  type: "WATCHING"
   }
 ];
 
@@ -29,11 +30,17 @@ export default class PresenceUpdate extends Task {
   public async run(): Promise<void> {
     const presence = Presences[Math.floor(Math.random() * Presences.length)];
 
+    let userCount = 0;
+    
+    Client.instance.guilds.cache.forEach((guild) => {
+      userCount += guild.memberCount;
+    });
+
     // @ts-ignore
     Client.instance.user?.setActivity({
       name: presence.name
         .replace("{SERVER_COUNT}", Client.instance.guilds.cache.size.toString())
-        .replace("{USER_COUNT}", Client.instance.users.cache.size.toString()),
+        .replace("{USER_COUNT}", formatNumbers(userCount)),
       type: ActivityTypes[presence.type]
     })
   }
