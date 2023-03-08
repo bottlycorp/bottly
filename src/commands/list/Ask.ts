de6de369a9a002ffbd6c460a25e8dae7706f42a6
Gaëtan H <steellgold0@gmail.com>
2023-03-08 21:30:45 +0100

    🧬 Introduce Trial (reload each month)

--- src/commands/list/Ask.ts
+++ src/commands/list/Ask.ts
@@ -2,10 +2,12 @@ import Client from "$core/Client";
 import Command from "$core/commands/Command";
 import { simpleEmbed } from "$core/utils/Embed";
 import { buildQuestion, AskContextOptions, Locales } from "$core/utils/Models";
+import { checkUser, getUser, updateUser } from "$core/utils/User";
 import { ask } from "$resources/messages.json";
 import { ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
 
 export default class Ask extends Command {
+
   public readonly slashCommand = new SlashCommandBuilder()
     .setName("ask")
     .setDescription(ask.command.description["en-US"])
@@ -27,23 +29,33 @@ export default class Ask extends Command {
       .addChoices(...Locales.map(l => ({ name: l.name, value: l.value }))));
 
   public async execute(command: ChatInputCommandInteraction): Promise<void> {
-    await command.deferReply();
+    await command.deferReply({ ephemeral: true });
+    await checkUser(command.user.id);
+    const user = await getUser(command.user.id);
+
+    if ((await getUser(command.user.id)).monthly == 0) {
+      command.editReply({ content: "Your free trial expired (wait next month)" });
+      return;
+    }
 
     const question = command.options.getString("question", true);
     const context = command.options.getString("context", false);
     const language = command.options.getString("language", false);
 
     const finalQuestion = buildQuestion(question, context ?? "default", language ?? command.locale);
-    console.log(finalQuestion);
 
     const response = await Client.instance.openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      max_tokens: 500,
+      max_tokens: 1500,
       temperature: 0.9,
-      messages: [{content: finalQuestion, name: "User", role: "user" }],
+      messages: [{ content: finalQuestion, name: "User", role: "user" }]
     });
 
     const text = response.data.choices[0].message?.content ?? "I don't know what to say...";
+
+    if (text !== "I don't know what to say...") {
+      await updateUser(command.user.id, { monthly: user.monthly - 1 });
+    }
 
     const embed = simpleEmbed(text, "normal", {
       text: command.user.username,
@@ -54,8 +66,18 @@ export default class Ask extends Command {
     const chatButton = new ButtonBuilder()
       .setCustomId("chat")
       .setLabel("Discussion")
-      .setStyle(ButtonStyle.Primary)
+      .setStyle(ButtonStyle.Primary);
+    const limitButton = new ButtonBuilder()
+      .setCustomId("limit")
+      .setLabel(`⛽ ${user.monthly - 1}/50 (monthly)`)
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(true);
+    const revealButton = new ButtonBuilder()
+      .setCustomId("reveal")
+      .setLabel("Reveal")
+      .setStyle(ButtonStyle.Secondary);
 
-    await command.editReply({ embeds: [embed], components: [{ type: 1, components: [chatButton] }] });
+    await command.editReply({ embeds: [embed], components: [{ type: 1, components: [chatButton, limitButton, revealButton] }] });
   }
+
 }
