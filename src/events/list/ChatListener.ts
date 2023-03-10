381ce4fae4b573caaa00d5fc9e0c00cba56df826
Gaëtan H <steellgold0@gmail.com>
2023-03-10 15:03:37 +0100

    💬 Interactions with chat

---
+++ src/events/list/ChatListener.ts
@@ -0,0 +1,52 @@
+import Event from "$core/events/Event";
+import { Message, MessageType } from "discord.js";
+import { checkThread, getThread, updateThread } from "$core/utils/Thread";
+import Client from "$core/Client";
+
+export default class ChatListener extends Event {
+
+  constructor() {
+    super("messageCreate", false);
+  }
+
+  public async execute(message: Message): Promise<void> {
+    if (!await checkThread(message.channel.id)) return;
+    if (message.author.bot) return;
+
+    const channel = message.channel;
+    if (!channel.isThread() || message.type !== MessageType.Default || !await checkThread(channel.id)) return;
+
+    const chat = await getThread(channel.id);
+    if (chat.active || chat.userId !== message.author.id) {
+      message.delete();
+      return;
+    }
+
+    await channel.sendTyping();
+    await updateThread(channel.id, {
+      active: true
+    });
+
+    chat.messages.push({ content: message.content, role: "user" });
+
+    if (message.content) {
+      const response = await Client.instance.openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        max_tokens: 1500,
+        temperature: 0.9,
+        messages: chat.messages
+      });
+
+      const content = response.data.choices[0].message?.content ?? "I don't know what to say...";
+
+      chat.messages.push({ content: content, role: "assistant" });
+
+      await updateThread(channel.id, chat);
+      await channel.send(content);
+    } else {
+      message.delete();
+      return;
+    }
+  }
+
+}
