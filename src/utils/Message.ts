520338e2a0f7cbc0bf2f96d71e95be0caefa798d
Gaëtan H <steellgold0@gmail.com>
2023-03-10 08:19:35 +0100

    🧾 Remove comments

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -1,6 +1,3 @@
-// Function to get a message in /resources/langs/{lang}.json
-// And a parameter ...args to replace the {0}, {1}, {2} ... in the message
-
 export const msg = (message: string, params: (number | string)[]): string => {
   const words = message.match(/\{[^}]+\}/g);
   if (!words) {
