89c20357fa2d9b4a9bda482d64a018c7528987d5
Gaëtan H <steellgold0@gmail.com>
2023-03-11 19:03:11 +0100

    📝 ESLint refactor + Stripe

--- src/Client.ts
+++ src/Client.ts
@@ -1,20 +1,25 @@
-import { Client as DiscordClient, GatewayIntentBits, Partials } from 'discord.js';
-import { Configuration, OpenAIApi } from 'openai';
-import Logger from '$core/utils/Logger';
-import CommandManager from '$core/commands/CommandManager';
-import EventManager from '$core/events/EventManager';
-import TaskManager from '$core/tasks/TaskManager';
+import { Client as DiscordClient, GatewayIntentBits, Partials } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+import Logger from "$core/utils/Logger";
+import CommandManager from "$core/commands/CommandManager";
+import EventManager from "$core/events/EventManager";
+import TaskManager from "$core/tasks/TaskManager";
 import "dotenv/config";
+import Stripe from "stripe";
 
 export default class Client extends DiscordClient {
 
-	public static instance: Client;
+  public static instance: Client;
 
   public readonly openai: OpenAIApi;
 
-	public readonly eventManager: EventManager;
-	public readonly commandManager: CommandManager;
+  public readonly eventManager: EventManager;
+
+  public readonly commandManager: CommandManager;
+
   public readonly taskManager: TaskManager;
+
+  public readonly stripe: Stripe;
 
   constructor() {
     super({
@@ -25,24 +30,29 @@ export default class Client extends DiscordClient {
       partials: [Partials.Message, Partials.Channel, Partials.Reaction]
     });
 
-		Client.instance = this;
-		this.login(process.env.TOKEN);
-    
+    Client.instance = this;
+    this.login(process.env.TOKEN);
+
     this.openai = new OpenAIApi(new Configuration({
       apiKey: process.env.OPEN_AI
     }));
+
+    this.stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY ?? "", {
+      apiVersion: "2022-11-15"
+    });
 
     this.eventManager = new EventManager();
     this.commandManager = new CommandManager();
     this.taskManager = new TaskManager();
 
-    this.on('ready', () => {
+    this.on("ready", () => {
       this.guilds.cache.forEach((guild) => {
         Logger.where(`${guild.name} (${guild.memberCount} members) | ID: ${guild.id}`);
       });
     });
-	}
+  }
+
 }
 
-Logger.info('The client is being initialized...');
+Logger.info("The client is being initialized...");
 new Client();
