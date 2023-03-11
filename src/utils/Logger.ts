b47a5bbd5c5adb57ca6de9e7122b48f6f60cb978
Gaëtan H <steellgold0@gmail.com>
2023-03-11 19:05:39 +0100

    📝 ESLint refactor

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,23 +1,23 @@
 import chalk from "chalk";
 
 export default {
-	info(message: string) {
-		console.log(chalk.yellow('[INFO]') + chalk.gray(': ') + chalk.reset(message));
-	},
+  info(message: string) {
+    console.log(chalk.yellow("[INFO]") + chalk.gray(": ") + chalk.reset(message));
+  },
 
-	success(message: string) {
-		console.log(chalk.green('[SUCCESS]') + chalk.gray(': ') + chalk.reset(message));
-	},
+  success(message: string) {
+    console.log(chalk.green("[SUCCESS]") + chalk.gray(": ") + chalk.reset(message));
+  },
 
-	error(message: string) {
-		console.log(chalk.red('[ERROR]') + chalk.gray(': ') + chalk.reset(message));
-	},
+  error(message: string) {
+    console.log(chalk.red("[ERROR]") + chalk.gray(": ") + chalk.reset(message));
+  },
 
-	request(message: string) {
-		console.log(chalk.blue('[REQUEST]') + chalk.gray(': ') + chalk.reset(message));
-	},
+  request(message: string) {
+    console.log(chalk.blue("[REQUEST]") + chalk.gray(": ") + chalk.reset(message));
+  },
 
-	where(message: string) {
-		console.log(chalk.blue('[WHERE I AM]') + chalk.gray(': ') + chalk.reset(message));
-	}
+  where(message: string) {
+    console.log(chalk.blue("[WHERE I AM]") + chalk.gray(": ") + chalk.reset(message));
+  }
 };
