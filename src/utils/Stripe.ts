c9b528c8507c24649e21355dddcfa72407e7598e
Gaëtan H <steellgold0@gmail.com>
2023-03-12 15:43:21 +0100

    Find all subscriptions

--- src/utils/Stripe.ts
+++ src/utils/Stripe.ts
@@ -10,3 +10,10 @@ export const findSubscriptionByEmail = async(email: string): Promise<Stripe.Subs
   if (subscription.data.length === 0) return null;
   return subscription.data[0];
 };
+
+export const findAllSubscriptionsByEmail = async(email: string): Promise<Stripe.Subscription[]> => {
+  const customer = await Client.instance.stripe.customers.list({ email });
+  if (customer.data.length === 0) return [];
+  const subscription = await Client.instance.stripe.subscriptions.list({ customer: customer.data[0].id });
+  return subscription.data;
+};
