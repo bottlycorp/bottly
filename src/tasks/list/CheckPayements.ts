b92466bad7f16d8d9628e397f9e9f2ce6d4195f8
Gaëtan H <steellgold0@gmail.com>
2023-03-12 16:18:27 +0100

    ⏲️ Check payements task

--- src/tasks/list/CheckPayements.ts
+++ src/tasks/list/CheckPayements.ts
@@ -1,8 +1,8 @@
 import Client from "$core/Client";
 import Task from "$core/tasks/Task";
-import Logger from "$core/utils/Logger";
 import { prisma } from "$core/utils/Prisma";
-import { typePremium } from "$core/utils/User";
+import { premiumRole } from "$core/utils/Roles";
+import { findAllSubscriptionsByEmail } from "$core/utils/Stripe";
 import "dotenv/config";
 
 export default class CheckPayements extends Task {
@@ -12,25 +12,33 @@ export default class CheckPayements extends Task {
   }
 
   public run(): void {
-    // Get status "all" and in live mode
-    Client.instance.stripe.subscriptions.list({
-      status: "all"
-    }).then((subscriptions) => {
-      subscriptions.data.forEach(async(subscription) => {
-        if (subscription.livemode == (process.env.STRIPE_LIVE_MODE ?? false)) return;
-        const customer = await Client.instance.stripe.customers.retrieve(subscription.customer as string);
+    const emails: string[] = [];
+    const customers = Client.instance.stripe.customers.list();
+    customers.then((customers) => {
+      customers.data.forEach((customer) => {
+        // @ts-ignore
+        emails.push(customer.email);
+      });
 
-        const user = await prisma.user.findFirst({ where: { email: customer.email } });
-
-        if (subscription.status !== "active" && subscription.status !== "trialing") {
-          if (user == null) return;
-          prisma.user.update({ where: { id: user.id }, data: { premium: false } });
+      emails.forEach(async(email) => {
+        const subscriptions = await findAllSubscriptionsByEmail(email);
+        if (subscriptions.map((subscription) => subscription.status).includes("active")
+          || subscriptions.map((subscription) => subscription.status).includes("trialing")) {
+          const user = await prisma.user.findFirst({ where: { email } });
+          if (!user) return;
+          premiumRole("add", user.id);
           return;
-        } else {
-          if (user == null) return;
+        }
 
-          const update = await prisma.user.update({ where: { id: user.id }, data: { premium: typePremium(subscription.status) } });
-          if (update == null) return;
+        if (subscriptions.length === 0) {
+          const user = prisma.user.update({
+            where: { email },
+            data: { premium: false }
+          });
+
+          user.then((user) => {
+            premiumRole("remove", user.id);
+          });
         }
       });
     });
