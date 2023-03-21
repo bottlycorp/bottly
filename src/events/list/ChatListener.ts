edae2d5e032c15b9fa89e485af4643e6fa3f6031
Gaëtan H <steellgold0@gmail.com>
2023-03-21 12:21:59 +0100

    🪄 Add exception and refactor some things

--- src/events/list/ChatListener.ts
+++ src/events/list/ChatListener.ts
@@ -2,6 +2,7 @@ import { Message, MessageType } from "discord.js";
 import { checkThread, getThread, updateThread } from "$core/utils/Thread";
 import Event from "$core/events/Event";
 import Client from "$core/Client";
+import { formatLinks } from "$core/utils/Message";
 
 export default class ChatListener extends Event {
 
@@ -11,68 +12,60 @@ export default class ChatListener extends Event {
 
   public async execute(message: Message): Promise<void> {
     if (!await checkThread(message.channel.id)) return;
-    if (message.author.bot) return;
+    const thread = await getThread(message.channel.id);
+
+    if (!thread) return;
+    if (thread.active) {
+      message.delete();
+      return;
+    }
 
     const channel = message.channel;
     if (!channel.isThread() || message.type !== MessageType.Default || !await checkThread(channel.id)) return;
 
-    const member = await channel.guild.members.fetch(Client.instance.user?.id ?? "1076862546658738236");
-    if (!member.permissions.has("ManageMessages")) {
-      await channel.send({ content: "I need the permission to manage messages, for delete optionals messages." });
+    if (message.author.bot && message.author.id !== Client.instance.user?.id) {
+      message.delete();
       return;
     }
 
-    const chat = await getThread(channel.id);
-    if (chat.active || chat.userId !== message.author.id) {
-      try {
-        message.delete();
-      } catch (error) {
-        channel.send("I don't have permission to delete messages, please contact the administrator of the server");
-      }
+    if (message.mentions.users.size > 0) return;
+    if (message.content.startsWith(".")) return;
+
+
+    if (message.author.id !== thread?.userId && message.author.id !== Client.instance.user?.id) {
+      message.delete();
       return;
     }
+
+    if (message.author.bot) return;
+    channel.sendTyping();
 
     await updateThread(channel.id, { active: true });
 
-    chat.messages.push({ content: message.content, role: "user" });
+    const response = await Client.instance.openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ content: message.content, role: "user" }],
+      max_tokens: 1500,
+      temperature: 0.9
+    });
 
-    if (message.content) {
-      try {
-        await channel.sendTyping();
-
-        const response = await Client.instance.openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          max_tokens: 1500,
-          temperature: 0.9,
-          messages: chat.messages
-        });
-
-        const content = response.data.choices[0].message?.content ?? "An error occurred while processing your request";
-
-        chat.messages.push({ content: content, role: "assistant" });
-        await updateThread(channel.id, chat);
-
-        if (content.length >= 1500) {
-          const split = content.split("\n");
-          const first = split.slice(0, split.length / 2).join("\n");
-          const second = split.slice(split.length / 2, split.length).join("\n");
-          await channel.send(first);
-          await channel.send(second);
-        } else {
-          await channel.send(content);
-        }
-      } catch (error) {
-        console.error(error);
-        channel.send("An error occurred while processing your request, please try again later, contact support");
-      }
-    } else {
-      try {
-        message.delete();
-      } catch (error) {
-        channel.send("I don't have permission to delete messages, please contact the administrator of the server");
-      }
+    if (!response.data.choices[0].message?.content) {
+      await message.reply(formatLinks("An error has occurred, or no response was found"));
       return;
     }
+
+    if (response.data.choices[0].message?.content?.length > 1500) {
+      const firstMessage = response.data.choices[0].message?.content?.slice(0, 1500);
+      const secondMessage = response.data.choices[0].message?.content?.slice(1500, response.data.choices[0].message?.content?.length);
+
+      await message.reply(formatLinks(firstMessage ?? "An error has occurred, or no response was found"));
+      await message.reply(formatLinks(secondMessage ?? "An error has occurred, or no response was found"));
+      return;
+    }
+
+    await message.reply(formatLinks(response.data.choices[0].message?.content)).then(() => {
+      updateThread(channel.id, { active: false });
+    });
   }
 
 }
