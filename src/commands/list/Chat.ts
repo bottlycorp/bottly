db665929f1c51937e76aeec2a72fd1208a3de77e
Gaëtan H <steellgold0@gmail.com>
2023-03-21 12:21:41 +0100

    🧩 Update the `/chat`

--- src/commands/list/Chat.ts
+++ src/commands/list/Chat.ts
@@ -1,13 +1,9 @@
-import { getUsageButton, simpleEmbed } from "$core/utils/Embed";
-import { limit, msg, getLang } from "$core/utils/Message";
-import { createThread } from "$core/utils/Thread";
-import { checkUser, getUser, isPremium } from "$core/utils/User";
 import { chat } from "$resources/messages.json";
-import { TextChannel, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
-import Client from "$core/Client";
 import Command from "$core/commands/Command";
-import dayjs from "dayjs";
+import { ActionRowBuilder, ChatInputCommandInteraction, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import "dotenv/config";
 import { prisma } from "$core/utils/Prisma";
+import { checkUser } from "$core/utils/User";
 
 export default class Ask extends Command {
 
@@ -17,90 +13,37 @@ export default class Ask extends Command {
     .setName("chat")
     .setDescription(chat.command.description["en-US"])
     .setDescriptionLocalizations({ fr: chat.command.description.fr })
-    .addStringOption(new SlashCommandStringOption()
-      .setName("question")
-      .setDescription(chat.command.options.question["en-US"])
-      .setDescriptionLocalizations({ fr: chat.command.options.question.fr })
-      .setRequired(true))
-    .addStringOption(new SlashCommandStringOption()
-      .setName("title")
-      .setDescription(chat.command.options.title["en-US"])
-      .setDescriptionLocalizations({ fr: chat.command.options.title.fr })
-      .setRequired(false))
     .setDMPermission(false);
 
   public async execute(command: ChatInputCommandInteraction): Promise<void> {
-    const question = command.options.getString("question", true);
-    await command.deferReply({ ephemeral: true });
+    const randomId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    if (!command.guild) return;
+
     await checkUser(command.user.id);
-    const user = await getUser(command.user.id);
-    const isPremiumUser = isPremium(user);
 
-    if (!isPremiumUser) {
-      if (user.chatUsage <= 0) {
-        command.editReply({
-          embeds: [simpleEmbed(chat.errors.trial[getLang(command.locale)], "error", { f: command.user })]
-        });
-        return;
-      }
-    }
-
-    const channel = command.channel;
-    if (!(channel instanceof TextChannel)) {
-      await command.editReply({ content: "You must execute this command in a text channel." });
-      return;
-    }
-
-    const member = await channel.guild.members.fetch(Client.instance.user?.id ?? "1076862546658738236");
-    if (!member.permissions.has("ManageMessages")) {
-      await command.editReply({ content: "I need the permission to manage messages, for delete optionals messages." });
-      return;
-    }
-
-    const response = await Client.instance.openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      max_tokens: 1500,
-      temperature: 0.9,
-      messages: [{ content: question, name: "User", role: "user" }]
-    });
-
-    const thread = await channel.threads.create({ name: limit(question, 50, "..."), autoArchiveDuration: 60, reason: "Chat with the bot" });
-    thread.members.add(command.user.id);
-
-    const responseText = response.data.choices[0].message?.content;
-    thread.send(msg(chat.command.messages.started[getLang(command.locale)], [question]));
-    thread.send(responseText ?? "An communication error with OpenAI has occured.");
-
-    await prisma.stats.create({
+    await prisma.thread.create({
       data: {
-        createdAt: dayjs().toDate(),
-        guildId: command.guild?.id ?? "DM",
+        modalId: randomId,
         userId: command.user.id,
-        type: "chat"
+        guildId: command.guild.id,
+        threadId: randomId // Temporary data, will be updated when the thread is created
       }
     });
 
-    await createThread(thread.id, {
-      userId: command.user.id,
-      guildId: command.guildId ?? "",
-      messages: [{ content: question, role: "user" }]
-    });
+    const modal = new ModalBuilder()
+      .setCustomId(randomId)
+      .setTitle("Démarrez une conversation")
+      .addComponents(
+        new ActionRowBuilder<TextInputBuilder>().addComponents(new TextInputBuilder()
+          .setCustomId("content")
+          .setStyle(TextInputStyle.Paragraph)
+          .setRequired(true)
+          .setMinLength(10)
+          .setLabel("La question initiale de la conversation")
+          .setPlaceholder("Qu'es ce que le 49.3"))
+      );
 
-    if (isPremiumUser) {
-      await command.editReply({
-        content: `Your chat has been created: ${thread}`
-      });
-    } else {
-      const user = await prisma.user.update({
-        where: { id: command.user.id },
-        data: { chatUsage: { decrement: 1 } }
-      });
-
-      await command.editReply({
-        content: `Your chat has been created: ${thread}`,
-        components: [{ type: 1, components: [getUsageButton(user.chatUsage)] }]
-      });
-    }
+    await command.showModal(modal);
   }
 
 }
