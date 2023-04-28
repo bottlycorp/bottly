4a791b6d0f688f736636c3ebeef50faea11c79e4
Gaëtan H <steellgold0@gmail.com>
2023-04-28 20:32:27 +0200

    feat(schema.prisma): add Message model to User model feat(message-create-chat.event.ts): add tokens and userId fields to Message model and update messages creation in updateDiscussion() function fix(message-create-chat.event.ts): fix tokens calculation in execute() function and add error handling in openai.createChatCompletion() function

--- src/events/message-create-chat/message-create-chat.event.ts
+++ src/events/message-create-chat/message-create-chat.event.ts
@@ -2,8 +2,8 @@ import { colors, openai } from "$core/client";
 import { chat } from "$core/commands/chat/chat.config";
 import { deleteCache, existCache, setCache } from "$core/utils/cache";
 import { global } from "$core/utils/config/message/command";
-import { translate } from "$core/utils/config/message/message.util";
-import { deleteDiscussion, getDiscussion, updateDiscussion } from "$core/utils/data/discussion";
+import { separate, translate } from "$core/utils/config/message/message.util";
+import { getDiscussion, updateDiscussion } from "$core/utils/data/discussion";
 import { DayJS } from "$core/utils/day-js";
 import { simpleEmbed } from "$core/utils/embed";
 import { limitString, userWithId } from "$core/utils/function";
@@ -50,16 +50,6 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
     return;
   }
 
-  updateDiscussion(thread.id, {
-    lastMessageAt: DayJS().unix(),
-    writing: true,
-    messages: {
-      create: {
-        message: message.content, role: "user"
-      }
-    }
-  });
-
   if (existCache(message.author.id)) {
     message.delete();
     return;
@@ -76,16 +66,22 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
     });
 
     colors.error(`${userWithId(message.author)} tried to speak in a thread but he has no more usages`);
-    await updateDiscussion(thread.id, { active: false, writing: false });
-
-    if (!user.privacy?.collectChat) {
-      await deleteDiscussion(thread.id);
-      thread.send({ embeds: [simpleEmbed(translate(toLocale(user.locale), chat.config.exec.deletedData), "info", "")] });
-    }
-
-    thread.setLocked(true);
+    await updateDiscussion(thread.id, { writing: false });
     return;
   }
+
+  updateDiscussion(thread.id, {
+    lastMessageAt: DayJS().unix(),
+    writing: true,
+    messages: {
+      create: {
+        createdAt: DayJS().unix(),
+        message: message.content, role: "user",
+        tokens: getTokens(message.content),
+        userId: user.userId
+      }
+    }
+  });
 
   setCache(message.author.id, true);
   thread.sendTyping();
@@ -104,7 +100,7 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
   const messages: { content: string; role: "user" | "system" | "assistant" }[] = [];
 
   discussion.messages.forEach(msg => {
-    tokens += getTokens(msg.message);
+    if (tokens <= 1200) tokens += getTokens(msg.message);
     messages.push({ content: msg.message, role: msg.role === "bot" ? "assistant" : msg.role });
   });
 
@@ -113,17 +109,22 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
     role: Role.user
   });
 
+  console.log(messages);
+
   await openai.createChatCompletion({
     messages: messages,
     model: "gpt-3.5-turbo",
-    max_tokens: tokens + 200
-  }).catch((error: Error) => {
+    max_tokens: tokens <= 200 ? tokens + 200 : tokens,
+    user: user.userId
+  }).catch(async(error: Error) => {
     clearInterval(interval);
-    console.error(error);
     message.reply({ embeds: [simpleEmbed(translate(toLocale(user.locale), global.config.exec.error, { error: error.message }), "error")] });
     deleteCache(message.author.id);
+    await updateDiscussion(thread.id, { writing: false });
+    colors.error(error.message);
   }).then(async(response) => {
     clearInterval(interval);
+
     const answer = response?.data.choices[0].message?.content;
     if (answer == null) {
       message.reply({ embeds: [simpleEmbed(translate(toLocale(user.locale), global.config.exec.error, {  error: "No answer" }), "error")] });
@@ -131,33 +132,40 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
     }
 
     deleteCache(message.author.id);
-    message.reply(limitString(answer, 2000)); // TODO: If the message is too long, split it in multiple messages
+    separate(answer).texts.forEach(async(msg) => {
+      thread.send(msg);
+    });
 
     await updateDiscussion(thread.id, {
       lastMessageAt: DayJS().unix(),
       writing: false,
       messages: {
         create: {
+          createdAt: DayJS().unix(),
+          tokens: getTokens(answer),
           message: answer,
-          role: Role.bot
+          role: Role.bot,
+          userId: user.userId
         }
       },
       count: { increment: 1 },
       user: { update: { usages: { update: { usage: { decrement: 1 } } } } }
-    }).catch((error: Error) => {
-      console.error(error);
+    }).catch(async(error: Error) => {
+      await updateDiscussion(thread.id, { writing: false });
+      colors.error(error.message);
     });
   });
 
   if (discussion?.title === "default") {
     await openai.createChatCompletion({
       messages: [{ role: "system", content: systemContext }, { content: message.content, role: "user" }],
-      max_tokens: 4000,
+      max_tokens: getTokens(systemContext) + getTokens(message.content),
       temperature: 0.4,
       model: "gpt-3.5-turbo"
-    }).catch((error: Error) => {
-      console.error(error);
+    }).catch(async(error: Error) => {
+      await updateDiscussion(thread.id, { writing: false });
       message.reply("An error occured while trying to resume your message");
+      colors.error(error.message);
     }).then(async(response) => {
       const answer = response?.data.choices[0].message?.content;
       if (answer == null) {
@@ -165,8 +173,8 @@ export const execute: EventExecute<"messageCreate"> = async(message: Message) =>
         return;
       }
 
-      if (await updateDiscussion(thread.id, { title: limitString(answer, 50) })) {
-        thread.setName(limitString(answer, 50));
+      if (await updateDiscussion(thread.id, { title: answer })) {
+        thread.setName(limitString(answer, 95));
       } else {
         message.reply("An error occured while trying to resume your message");
       }
