c0bd6d834e1d9e5e96fb97f6fc33cd686e84bb0d
Gaëtan H <steellgold0@gmail.com>
2023-05-21 18:55:19 +0200

    feat(chat.config.ts): add German translations to the chat messages

--- src/commands/chat/chat.config.ts
+++ src/commands/chat/chat.config.ts
@@ -11,7 +11,8 @@ export const chat = {
       "pt-BR": "Comando principal da conversa com Bottly",
       ru: "Основная команда диалоговой нити с Bottly",
       uk: "Основна команда діалогової нитки з Bottly",
-      "es-ES": "Comando principal del hilo de diálogo con Bottly"
+      "es-ES": "Comando principal del hilo de diálogo con Bottly",
+      de: "Hauptbefehl des Dialogthreads mit Bottly"
     },
     subcmds: {
       stop: {
@@ -24,7 +25,8 @@ export const chat = {
           "pt-BR": "Pare a conversa atual",
           ru: "Остановить текущую диалоговую нить",
           uk: "Зупинити поточну діалогову нитку",
-          "es-ES": "Detener el hilo de diálogo actual"
+          "es-ES": "Detener el hilo de diálogo actual",
+          de: "Stoppen Sie den aktuellen Dialogthread"
         }
       },
       download: {
@@ -37,7 +39,8 @@ export const chat = {
           "pt-BR": "Baixe a conversa atual (uma vez concluída)",
           ru: "Загрузить текущую диалоговую нить (после завершения)",
           uk: "Завантажити поточну діалогову нитку (після завершення)",
-          "es-ES": "Descargue el hilo de diálogo actual (una vez finalizado)"
+          "es-ES": "Descargue el hilo de diálogo actual (una vez finalizado)",
+          de: "Laden Sie den aktuellen Dialogthread herunter (nach Abschluss)"
         }
       },
       talk: {
@@ -55,7 +58,8 @@ export const chat = {
               "pt-BR": "Contexto da conversa",
               ru: "Контекст диалоговой нити",
               uk: "Контекст діалогової нитки",
-              "es-ES": "Contexto del hilo de diálogo"
+              "es-ES": "Contexto del hilo de diálogo",
+              de: "Kontext des Dialogthreads"
             }
           },
           prompt: {
@@ -68,7 +72,8 @@ export const chat = {
               "pt-BR": "Pergunta inicial da conversa",
               ru: "Начальный вопрос диалоговой нити",
               uk: "Початкове запитання діалогової нитки",
-              "es-ES": "Pregunta inicial del hilo de diálogo"
+              "es-ES": "Pregunta inicial del hilo de diálogo",
+              de: "Initiale Frage des Dialogthreads"
             }
           },
           private: {
@@ -81,7 +86,8 @@ export const chat = {
               "pt-BR": "Se a conversa é privada",
               ru: "Если диалоговая нить является частной",
               uk: "Якщо діалогова нитка є приватною",
-              "es-ES": "Si el hilo de diálogo es privado"
+              "es-ES": "Si el hilo de diálogo es privado",
+              de: "Wenn der Dialogthread privat ist"
             }
           }
         },
@@ -91,7 +97,8 @@ export const chat = {
           "pt-BR": "Abra um diálogo rápido com Bottly",
           ru: "Быстро откройте диалоговую нить с Bottly",
           uk: "Швидко відкрийте діалогову нитку з Bottly",
-          "es-ES": "Abra un hilo de diálogo rápidamente con Bottly"
+          "es-ES": "Abra un hilo de diálogo rápidamente con Bottly",
+          de: "Öffnen Sie einen Dialogthread schnell mit Bottly"
         }
       }
     },
@@ -103,7 +110,8 @@ export const chat = {
           "pt-BR": "Não mostre este conselho novamente",
           ru: "Больше не показывать эту подсказку",
           uk: "Більше не показувати цей підказку",
-          "es-ES": "No vuelva a mostrar este consejo"
+          "es-ES": "No vuelva a mostrar este consejo",
+          de: "Diesen Tipp nicht mehr anzeigen"
         }
       },
       notPremiumDownload: {
@@ -112,7 +120,8 @@ export const chat = {
         "pt-BR": "Você não pode baixar a conversa porque não é um usuário premium.",
         ru: "Вы не можете загрузить диалоговую нить, потому что вы не являетесь премиум-пользователем.",
         uk: "Ви не можете завантажити діалогову нитку, тому що ви не є преміум-користувачем.",
-        "es-ES": "No puede descargar el hilo de diálogo porque no es un usuario premium."
+        "es-ES": "No puede descargar el hilo de diálogo porque no es un usuario premium.",
+        de: "Sie können den Dialogthread nicht herunterladen, da Sie kein Premium-Benutzer sind."
       },
       channelTemporaryTitle: {
         "en-US": "Discussion with {user}",
@@ -120,7 +129,8 @@ export const chat = {
         "pt-BR": "Conversa com {user}",
         ru: "Обсуждение с {user}",
         uk: "Обговорення з {user}",
-        "es-ES": "Discusión con {user}"
+        "es-ES": "Discusión con {user}",
+        de: "Diskussion mit {user}"
       },
       channelCreating: {
         "en-US": "{emojiTyping} Your discussion is being created please wait...",
@@ -128,7 +138,8 @@ export const chat = {
         "pt-BR": "{emojiTyping} Sua conversa está sendo criada, por favor, aguarde...",
         ru: "{emojiTyping} Ваша диалоговая нить создается, подождите...",
         uk: "{emojiTyping} Ваша діалогова нитка створюється, зачекайте...",
-        "es-ES": "{emojiTyping} Su hilo de diálogo se está creando, espere..."
+        "es-ES": "{emojiTyping} Su hilo de diálogo se está creando, espere...",
+        de: "{emojiTyping} Ihr Dialogthread wird erstellt, bitte warten Sie..."
       },
       alreadyActiveDiscussion: {
         "en-US": "You already have an active discussion, you can stop it with the command {chatStop} in the <#{thread}> channel",
@@ -136,7 +147,8 @@ export const chat = {
         "pt-BR": "Você já tem uma conversa ativa, você pode pará-la com o comando {chatStop} no canal <#{thread}>",
         ru: "У вас уже есть активное обсуждение, вы можете остановить его с помощью команды {chatStop} в канале <#{thread}>",
         uk: "У вас вже є активне обговорення, ви можете зупинити його за допомогою команди {chatStop} в каналі <#{thread}>",
-        "es-ES": "Ya tiene un hilo de discusión activo, puede detenerlo con el comando {chatStop} en el canal <#{thread}>"
+        "es-ES": "Ya tiene un hilo de discusión activo, puede detenerlo con el comando {chatStop} en el canal <#{thread}>",
+        de: "Sie haben bereits eine aktive Diskussion, Sie können sie mit dem Befehl {chatStop} im Kanal <#{thread}> stoppen"
       },
       notHaveActiveDiscussion: {
         "en-US": "You don't have an active discussion, you can start one with the command {chatTalk}",
@@ -144,7 +156,8 @@ export const chat = {
         "pt-BR": "Você não tem uma conversa ativa, você pode começar uma com o comando {chatTalk}",
         ru: "У вас нет активного обсуждения, вы можете начать одно с помощью команды {chatTalk}",
         uk: "У вас немає активного обговорення, ви можете почати одне за допомогою команди {chatTalk}",
-        "es-ES": "No tiene un hilo de discusión activo, puede iniciar uno con el comando {chatTalk}"
+        "es-ES": "No tiene un hilo de discusión activo, puede iniciar uno con el comando {chatTalk}",
+        de: "Sie haben keine aktive Diskussion, Sie können mit dem Befehl {chatTalk} eine starten"
       },
       notTheAuthor: {
         "en-US": "You are not the author of this discussion so you can stop it or download it",
@@ -152,7 +165,8 @@ export const chat = {
         "pt-BR": "Você não é o autor desta conversa, portanto não pode pará-la ou baixá-la",
         ru: "Вы не автор этого обсуждения, поэтому не можете остановить его или загрузить",
         uk: "Ви не автор цього обговорення, тому не можете зупинити його або завантажити",
-        "es-ES": "No eres el autor de esta discusión, por lo que no puedes detenerla o descargarla"
+        "es-ES": "No eres el autor de esta discusión, por lo que no puedes detenerla o descargarla",
+        de: "Sie sind nicht der Autor dieser Diskussion, daher können Sie sie nicht stoppen oder herunterladen"
       },
       stopped: {
         "en-US": "The discussion has been stopped correctly, there was a total of `{count}` messages exchanged.",
@@ -160,7 +174,8 @@ export const chat = {
         "pt-BR": "A conversa foi parada corretamente, houve um total de `{count}` mensagens trocadas.",
         ru: "Обсуждение было остановлено правильно, было обменено `{count}` сообщений.",
         uk: "Обговорення було зупинено правильно, було обмінено `{count}` повідомлень.",
-        "es-ES": "La discusión se ha detenido correctamente, hubo un total de `{count}` mensajes intercambiados."
+        "es-ES": "La discusión se ha detenido correctamente, hubo un total de `{count}` mensajes intercambiados.",
+        de: "Die Diskussion wurde korrekt gestoppt, es wurden insgesamt `{count}` Nachrichten ausgetauscht."
       },
       creatingFile: {
         "en-US": "{emojiTyping} Your discussion is being downloaded, please wait...",
@@ -168,7 +183,8 @@ export const chat = {
         "pt-BR": "{emojiTyping} Sua conversa está sendo baixada, por favor, aguarde...",
         ru: "{emojiTyping} Ваша диалоговая нить загружается, подождите...",
         uk: "{emojiTyping} Ваша діалогова нитка завантажується, зачекайте...",
-        "es-ES": "{emojiTyping} Su hilo de diálogo se está descargando, espere..."
+        "es-ES": "{emojiTyping} Su hilo de diálogo se está descargando, espere...",
+        de: "{emojiTyping} Ihr Dialogthread wird heruntergeladen, bitte warten Sie..."
       },
       createdFile: {
         "en-US": "Your discussion has been downloaded you can download it with the file below",
@@ -176,7 +192,8 @@ export const chat = {
         "pt-BR": "Sua conversa foi baixada, você pode baixá-la com o arquivo abaixo",
         ru: "Ваша диалоговая нить была загружена, вы можете загрузить ее с помощью файла ниже",
         uk: "Ваша діалогова нитка була завантажена, ви можете завантажити її за допомогою файлу нижче",
-        "es-ES": "Su hilo de diálogo ha sido descargado, puede descargarlo con el archivo a continuación"
+        "es-ES": "Su hilo de diálogo ha sido descargado, puede descargarlo con el archivo a continuación",
+        de: "Ihr Dialogthread wurde heruntergeladen, Sie können ihn mit der folgenden Datei herunterladen"
       },
       downloadCommand: {
         "en-US": "If you are a **Premium {emojiPremium}** user you can use the {chatDownload} command to get the conversation in HTML format",
@@ -193,7 +210,11 @@ export const chat = {
           "Якщо ви є користувачем **Premium {emojiPremium}**, ви можете використовувати команду {chatDownload}, щоб отримати",
           "розмову в форматі HTML"
         ].join(" "),
-        "es-ES": "Si es un usuario **Premium {emojiPremium}**, puede usar el comando {chatDownload} para obtener la conversación en formato HTML"
+        "es-ES": "Si es un usuario **Premium {emojiPremium}**, puede usar el comando {chatDownload} para obtener la conversación en formato HTML",
+        de: [
+          "Wenn Sie ein **Premium {emojiPremium}**-Benutzer sind, können Sie den Befehl {chatDownload} verwenden, um das Gespräch im",
+          "HTML-Format zu erhalten"
+        ].join(" ")
       },
       private: {
         "en-US": "private",
@@ -201,7 +222,8 @@ export const chat = {
         "pt-BR": "privada",
         ru: "частный",
         uk: "приватний",
-        "es-ES": "privado"
+        "es-ES": "privado",
+        de: "privat"
       },
       public: {
         "en-US": "public",
@@ -209,7 +231,8 @@ export const chat = {
         "pt-BR": "pública",
         ru: "общественное",
         uk: "публічний",
-        "es-ES": "público"
+        "es-ES": "público",
+        de: "öffentlich"
       },
       channelCreated: {
         "en-US": "Your `{type}` discussion is created, you can start talking to me in the <#{id}> channel",
@@ -217,7 +240,8 @@ export const chat = {
         "pt-BR": "Sua conversa `{type}` foi criada, você pode começar a falar comigo no canal <#{id}>",
         ru: "Ваша диалоговая нить `{type}` создана, вы можете начать разговаривать со мной в канале <#{id}>",
         uk: "Ваша діалогова нитка `{type}` створена, ви можете почати розмовляти зі мною в каналі <#{id}>",
-        "es-ES": "Su hilo de diálogo `{type}` se ha creado, puede comenzar a hablar conmigo en el canal <#{id}>"
+        "es-ES": "Su hilo de diálogo `{type}` se ha creado, puede comenzar a hablar conmigo en el canal <#{id}>",
+        de: "Ihr `{type}`-Diskussionsthread wurde erstellt, Sie können in den Kanal <#{id}> starten"
       },
       channelNotCreated: {
         "en-US": "Your `{type}` discussion is failed to create, please try again later or check permissions",
@@ -225,7 +249,8 @@ export const chat = {
         "pt-BR": "Seu thread `{type}` não pôde ser criado, tente novamente mais tarde ou verifique as permissões",
         ru: "Ваше обсуждение `{type}` не удалось создать, пожалуйста, повторите попытку позже или проверьте разрешения",
         uk: "Ваше обговорення `{type} не вдалося створити, будь ласка, спробуйте пізніше або перевірте дозволи",
-        "es-ES": "No se ha podido crear el debate `{type}`, inténtelo de nuevo más tarde o compruebe los permisos."
+        "es-ES": "No se ha podido crear el debate `{type}`, inténtelo de nuevo más tarde o compruebe los permisos.",
+        de: "Ihr Thema `{type}` konnte nicht erstellt werden, bitte versuchen Sie es später noch einmal oder überprüfen Sie die Berechtigungen"
       },
       deletedData: {
         "en-US": [
@@ -251,6 +276,10 @@ export const chat = {
         "es-ES": [
           ":wave: <@{id}> Los datos de esta discusión se han eliminado porque ha establecido que no desea que sus datos se guarden en su",
           "configuraciones de privacidad."
+        ].join(" "),
+        de: [
+          ":wave: <@{id}> Die Daten dieser Diskussion wurden gelöscht, da Sie festgelegt haben, dass Ihre Daten nicht gespeichert werden sollen",
+          "Datenschutzeinstellungen."
         ].join(" ")
       },
       discussionOpened: {
@@ -295,6 +324,13 @@ export const chat = {
           ["- Puedes hablar con otras personas poniendo un `.` antes de tu mensaje o si tu mensaje contiene `@mention` (o responder",
             "a un mensaje con ping activado)"].join(" "),
           "- Cada mensaje que envíes cuenta como 1 uso (verifica tu cuota con el comando {history})"
+        ].join("\n"),
+        de: [
+          ":wave: Ich bin hier, um dir zu helfen, du kannst anfangen mit mir zu reden\n",
+          "- Wenn Sie die Diskussion beenden möchten, verwenden Sie den Befehl {chatStop}",
+          ["- Sie können mit anderen Personen sprechen, indem Sie ein `.` vor Ihrer Nachricht platzieren oder wenn Ihre Nachricht `@mention` enthält",
+            "(oder auf eine Nachricht mit aktiviertem Ping antworten)"].join(" "),
+          "- Jede Nachricht, die Sie senden, zählt als 1 Verwendung (überprüfen Sie Ihr Kontingent mit dem Befehl {history})"
         ].join("\n")
       },
       discussionOpenedPremium: {
@@ -303,7 +339,8 @@ export const chat = {
         "pt-BR": "{emojiPremium} Depois de parar a conversa, você pode baixá-la no formato HTML",
         ru: "{emojiPremium} После остановки обсуждения вы сможете загрузить его в формате HTML",
         uk: "{emojiPremium} Після зупинки обговорення ви зможете завантажити його у форматі HTML",
-        "es-ES": "{emojiPremium} Una vez que haya detenido la discusión, podrá descargarla en formato HTML"
+        "es-ES": "{emojiPremium} Una vez que haya detenido la discusión, podrá descargarla en formato HTML",
+        de: "{emojiPremium} Sobald Sie die Diskussion gestoppt haben, können Sie sie im HTML-Format herunterladen"
       },
       premiumTip: {
         "en-US": "With **Premium {emojiPremium}** you can download the discussion in HTML format as soon as you stop it",
@@ -311,7 +348,8 @@ export const chat = {
         "pt-BR": "Com **Premium {emojiPremium}** você pode baixar a conversa no formato HTML assim que pará-la",
         ru: "С **Premium {emojiPremium}** вы можете загрузить обсуждение в формате HTML, как только остановите его",
         uk: "З **Premium {emojiPremium}** ви можете завантажити обговорення в форматі HTML, як тільки зупините його",
-        "es-ES": "Con **Premium {emojiPremium}** puede descargar el hilo de diálogo en formato HTML tan pronto como lo detenga"
+        "es-ES": "Con **Premium {emojiPremium}** puede descargar el hilo de diálogo en formato HTML tan pronto como lo detenga",
+        de: "Mit **Premium {emojiPremium}** können Sie den Dialogthread im HTML-Format herunterladen, sobald Sie ihn stoppen"
       },
       premiumTipAre: {
         "en-US": "You are a **Premium {emojiPremium}** user, you can download the discussion in HTML format",
@@ -334,6 +372,10 @@ export const chat = {
         "es-ES": [
           "Eres un usuario **Premium {emojiPremium}**, puedes descargar la discusión en formato",
           "HTML haciendo clic en el botón de abajo"
+        ].join(" "),
+        de: [
+          "Sie sind ein **Premium {emojiPremium}**-Benutzer, Sie können den Dialogthread im Format",
+          "HTML herunterladen, indem Sie auf die Schaltfläche unten klicken"
         ].join(" ")
       }
     }
