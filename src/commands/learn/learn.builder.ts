fd930ea658ba9db9adfb0ffb87dfc1076b4866b1
Gaëtan H <steellgold0@gmail.com>
2023-05-22 21:11:19 +0200

    refactor(learn.builder.ts): rename support variable to learn refactor(learn.cmd.ts): add support for selecting interests feat(learn.config.ts): add support for selecting interests and enable/disable buttons

--- src/commands/learn/learn.builder.ts
+++ src/commands/learn/learn.builder.ts
@@ -1,4 +1,4 @@
-import { support } from "./learn.config";
+import { learn } from "./learn.config";
 import { EnableInDev } from "$core/utils/handler";
 import { SlashCommandDefition } from "$core/utils/handler/command";
 import { SlashCommandBuilder } from "discord.js";
@@ -6,6 +6,6 @@ import { SlashCommandBuilder } from "discord.js";
 export const enableInDev: EnableInDev = true;
 
 export const slashCommand: SlashCommandDefition = new SlashCommandBuilder()
-  .setName(support.config.name["en-US"])
-  .setDescription(support.config.description["en-US"])
-  .setDescriptionLocalizations(support.config.description);
+  .setName(learn.config.name["en-US"])
+  .setDescription(learn.config.description["en-US"])
+  .setDescriptionLocalizations(learn.config.description);
--- src/commands/learn/learn.cmd.ts
+++ src/commands/learn/learn.cmd.ts
@@ -1,17 +1,47 @@
 import { translate } from "$core/utils/config/message/message.util";
-import { simpleEmbed } from "$core/utils/embed";
+import { simpleButton, simpleEmbed, simpleSelect } from "$core/utils/embed";
 import { CommandExecute } from "$core/utils/handler/command";
-import { support } from "./learn.config";
+import { ButtonStyle } from "discord.js";
+import { learn } from "./learn.config";
+import { interests } from "./interests.config";
 
 export const execute: CommandExecute = async(command) => {
-  const embedImage = simpleEmbed(undefined, "info", undefined, undefined, undefined, translate(command.locale, support.config.imgs));
+  const embedImage = simpleEmbed(undefined, "info", undefined, undefined, undefined, translate(command.locale, learn.config.imgs));
   const embedText = simpleEmbed(
-    translate(command.locale, support.config.exec.embed.description),
+    translate(command.locale, learn.config.exec.embed.description),
     "info",
-    translate(command.locale, support.config.exec.embed.title)
+    translate(command.locale, learn.config.exec.embed.title)
   );
 
-  command.editReply({
-    embeds: [embedImage, embedText]
+  const embedInterests = simpleEmbed(
+    translate(command.locale, learn.config.exec.embed.descriptionInterests),
+    "info",
+    translate(command.locale, learn.config.exec.embed.titleInterests)
+  );
+
+  const enableBtn = simpleButton(translate(command.locale, learn.config.buttons.enable), ButtonStyle.Secondary, "enable", false, { name: "🔓" });
+
+  const message = await command.editReply({
+    embeds: [embedImage, embedText],
+    components: [{ type: 1, components: [enableBtn] }]
+  });
+
+  const collector = message.createMessageComponentCollector({ time: 60000 });
+  collector.on("collect", async(i) => {
+    i.deferUpdate();
+    if (i.customId === "enable") {
+      await command.editReply({
+        embeds: [embedInterests],
+        components: [{ type: 1, components: [simpleSelect(
+          "interests", "Select your interests", Array.from(Object.keys(interests)).map((interest) => {
+            return {
+              label: translate(command.locale, interests[interest].localization),
+              value: interest,
+              emoji: { name: interests[interest].emoji }
+            };
+          })
+        )] }]
+      });
+    }
   });
 };
